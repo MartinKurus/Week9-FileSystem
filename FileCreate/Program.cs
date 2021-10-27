2085cbb89ccf3181345abd13fa9a4510a1199eb2
Opilane TTHK <opilane@tthk.ee>
2021-10-27 09:43:31 +0300

    first commit

---
+++ FileCreate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples\wishlist";
+            string fileName = "wishlist";
+            string fullFilePath = $@"{rootDirectory}\{fileName}.txt";
+            Console.WriteLine(fullFilePath);
+
+            bool directoryExists = Directory.Exists(rootDirectory);
+
+            bool fileExists = File.Exists(fullFilePath);
+
+            if(directoryExists && fileExists)
+            {
+                Console.WriteLine($"File {fileName} already exists in {rootDirectory}.");
+            }
+            else if (!directoryExists)
+            {
+                Console.WriteLine($"wishlist directory does not exist.");
+                Directory.CreateDirectory(rootDirectory);
+                File.Create(fullFilePath);
+                
+            }
+            else
+            {
+                File.Create(fullFilePath);
+                Console.WriteLine($"File {fileName}.txt has been created.");
+            }
+            
+        }
+    }
+}
---
+++ FileSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            Console.WriteLine("Enter directory name:");
+            string userDirectory = Console.ReadLine();
+
+            string newDirectoryFullPath = @$"{rootDirectory}\{userDirectory}";
+            Console.WriteLine(newDirectoryFullPath);
+
+            bool directoryExists = Directory.Exists(newDirectoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} already exists in {rootDirectory}");
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryFullPath);
+                Console.WriteLine($"Directory {userDirectory} has been created.");
+            }
+
+        }
+    }
+}
